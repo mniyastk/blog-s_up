d672d22b953c451646508b81964145aee328d9af
Bijeesh M <bijeeshh.m@gmail.com>
2024-02-08 17:25:33 +0530

    feat:added some files

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
+const userRoute = require("./routes/userRoute");
 const mongoose = require("mongoose");
-require("dotenv").config()
+require("dotenv").config();
 
-app.listen(3005,()=>{
-    console.log("server running ");
-})
+mongoose
+  .connect("mongodb+srv://blogsup:09779346@cluster0.9iogxhx.mongodb.net/blog'sUp")
+  .then(() => console.log("db connected"))
+  .catch((err) => {
+    console.log(err);
+  });
 
+app.use(express.json());
+
+app.use("/user", userRoute);
+
+app.listen(3005, () => {
+  console.log("server running ");
+});
