261a71d0ac6bd2fece0bd98e872763acbd92f463
SanuRaj-K <sanusrk29@gmail.com>
2024-02-08 15:55:13 +0530

    feat:added some files and folders

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-require("dotenv").config()
+const authorRoute = require("./routes/authorRoute");
+require("dotenv").config();
 
-app.listen(3005,()=>{
-    console.log("server running ");
-})
+app.use("/author", authorRoute);
 
+mongoose.connect("mongodb://localhost:27017/blogsup");
+
+app.listen(3005, () => {
+  console.log("server running ");
+});
