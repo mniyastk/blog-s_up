adb79467aae83e7de63ee33f4d106bb09c91806f
SanuRaj-K <sanusrk29@gmail.com>
2024-02-09 12:14:06 +0530

    change

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const app = express();
 const mongoose = require("mongoose");
 const authorRoute = require("./routes/authorRoute");
 require("dotenv").config();
-
+mongoose
+  .connect("mongodb+srv://blogsup:09779346@cluster0.9iogxhx.mongodb.net/blog'sUp")
+  .then(() => console.log("mongodb connected"))
+  .catch((err) => console.log(err));
+app.use(express.json());
 app.use("/author", authorRoute);
-
-mongoose.connect("mongodb://localhost:27017/blogsup");
 
 app.listen(3005, () => {
   console.log("server running ");
-});
+}); 
+  
