ef7efdba734103252722a97187d83183d9e838b2
Bijeesh M <bijeeshh.m@gmail.com>
2024-02-16 16:44:16 +0530

    fix: fixed the bug

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const userRoute = require("./routes/userRoute");
 const authorRoute = require("./routes/authorRoute");
+const adminRoute = require("./routes/adminRoute");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 
@@ -19,8 +20,9 @@ mongoose
 app.use(express.json());
 
 app.use("/author", authorRoute);
+app.use("/user", userRoute);
+app.use("/admin", adminRoute);
 
 app.listen(3005, () => {
   console.log("server running ");
 });
- 
