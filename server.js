eca700da08856b4911d5ba47fb96b635716b69c6
Bijeesh M <bijeeshh.m@gmail.com>
2024-04-30 12:26:50 +0530

    updated git ignore file

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://blogsup.vercel.app/",
     credentials: true,
   })
 );
