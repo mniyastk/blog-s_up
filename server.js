b028cc2c8cc022ea73cdc68e040becff9bf02fd1
Muhammed Niyas Tk <139123396+mniyastk@users.noreply.github.com>
2024-05-02 15:23:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "https://blogsup.vercel.app",
+    origin: "https://blogsup.shop",
     credentials: true,
   })
 );
