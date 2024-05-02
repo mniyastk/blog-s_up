04ddd87a806c9c08d15fcc9abe7668193d06a418
Muhammed Niyas Tk <139123396+mniyastk@users.noreply.github.com>
2024-05-02 15:37:42 +0530

    Update s3.js

--- s3.js
+++ s3.js
@@ -1,10 +1,8 @@
-require("dotenv").config();
-
 const AWS = require("aws-sdk");
 
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION,
 });
 
