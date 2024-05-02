e1b57bb3f0add1e8c18c558d3dd5e8b92ada0359
Muhammed Niyas Tk <139123396+mniyastk@users.noreply.github.com>
2024-05-02 16:24:01 +0530

    Update s3.js

--- s3.js
+++ s3.js
@@ -1,9 +1,9 @@
 const AWS = require("aws-sdk");
 
 AWS.config.update({
-  accessKeyId: AKIA4MTWM4LJU2PGTFP2,
-  secretAccessKey: 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,
-  region:us-east-1,
+  accessKeyId: "AKIA4MTWM4LJU2PGTFP2",
+  secretAccessKey: "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",
+  region:"us-east-1",
 });
 
 const s3 = new AWS.S3();
