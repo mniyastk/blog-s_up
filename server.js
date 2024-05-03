c154a840700b8f8191392e44cc363e6ed88ebdea
SanuRaj-K <sanusrk29@gmail.com>
2024-05-03 10:26:47 +0530

    feat:changes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "https://blogsup.vercel.app/",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
@@ -32,9 +32,9 @@ try {
   console.log(error.message);
 }
 
-app.use("/api/author", authorRoute);
-app.use("/api/user", userRoute); 
-app.use("/api/admin", adminRoute);
+app.use("/author", authorRoute);
+app.use("/user", userRoute); 
+app.use("/admin", adminRoute);
 
 app.listen(3005, () => {
   console.log("server running ");
